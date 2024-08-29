54c054f2e8b513c0f9d4df1c75229db554c4551e
Sean Hansen <sean.hansen826@my.tccd.edu>
2024-08-28 21:17:45 -0500

    notes over identifiers, types, assignments and operators

--- classwork/Chapter2/VariablesAndTypes/VariablesAndTypes.cpp
+++ classwork/Chapter2/VariablesAndTypes/VariablesAndTypes.cpp
@@ -20,7 +20,7 @@ int main()
     //File Paths
     std::cout << "C:\\Windows\\Temp";
 
-    //Variables - named memore location to store data
+    //Variables - named memory location to store data
 
     //decleration - tell compiler a variable exists
     //scope - lifetime of a variable
@@ -30,7 +30,39 @@ int main()
     int releaseYear = 1900;
 
     int rating = 3, stars =5;
-
+        
 
     std::cout << length;
+
+    //Identifiers
+    //int _hours, _1stGrade, 1stGrade, 1stGrade, first-grade;
+    int examGrade = 0, hoursWorked = 0;
+    std::cout << examGrade;
+
+    //Types
+    // Define: What kind of data is stored, range, and size
+
+ // double and float
+    double largeNumber = 10E100;
+
+    int intSize = sizeof(int);
+
+
+
+
+    //assignment
+    //asn_op ::=V = E
+    //Must be type compatible (same type between the two)
+
+    int shapeLength = 10;
+    int shapeWidth = 20;
+    int shapeArea;
+
+
+    shapeArea = shapeLength;
+    shapeWidth = 10;
+    // does not work |  20 = shapeWidth;
+    int remainder = 10 % 3; // 1
+
+    double dresult = 10.0 / 3.0; //3.333
 }
