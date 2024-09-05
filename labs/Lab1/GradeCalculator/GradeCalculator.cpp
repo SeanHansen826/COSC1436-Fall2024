060f920904044fef51829e182c0cd5d3367ffe88
Sean Hansen <sean.hansen826@my.tccd.edu>
2024-09-05 15:38:23 -0500

    completed story 5 (introduced #iomanip

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -3,13 +3,13 @@
 //COSC1436-Fall2024
 
 #include<iostream>
+#include<iomanip>
 
 int main()
 {
     std::cout << "Lab 1\n";
     std::cout << "Sean Hansen\n";
     std::cout << "COSC1436-Fall2024\n";
-    std::cout << std::endl;
 
     std::string userName;
 
@@ -36,7 +36,7 @@ int main()
     double exam2Grade = 0;
     double exam3Grade = 0;
     
-    std::cout << "Please Individually Input 3 \"Exam\" Grades:\n";
+    std::cout << "Please Input 3 \"Exam\" Grades:\n";
     std::cout << "Exam 1:\t";
     std::cin >> exam1Grade;
     std::cout << "Exam 2:\t";
@@ -44,39 +44,25 @@ int main()
     std::cout << "Exam 3:\t";
     std::cin >> exam3Grade;  
 
-    std::cout << "Lab Grades for: " + userName;
+    std::cout << "Lab Grades for:\t" << userName;
     std::cout << std::endl;
-    std::cout << "Lab 1:\t";
-    std::cout << lab1Grade;
-    std::cout << std::endl;
-    std::cout << "Lab 2:\t";
-    std::cout << lab2Grade;
-    std::cout << std::endl;
-    std::cout << "Lab 3:\t";
-    std::cout << lab3Grade;
-    std::cout << std::endl;
-    std::cout << "Lab 4:\t";
-    std::cout << lab4Grade;
-    std::cout << std::endl;
-    std::cout << "Exam 1:\t";
-    std::cout << exam1Grade;
-    std::cout << std::endl;
-    std::cout << "Exam 2:\t";
-    std::cout << exam2Grade;
-    std::cout << std::endl;
-    std::cout << "Exam 3:\t";
-    std::cout << exam3Grade;
-    std::cout << std::endl;
+    std::cout << "Lab 1:\t" << lab1Grade << std::endl;
+    std::cout << "Lab 2:\t" << lab2Grade << std::endl;
+    std::cout << "Lab 3:\t" << lab3Grade << std::endl; 
+    std::cout << "Lab 4:\t" << lab4Grade << std::endl; 
+    
+    std::cout << "Exam 1:\t" << exam1Grade << std::endl;
+    std::cout << "Exam 2:\t" << exam2Grade << std::endl;
+    std::cout << "Exam 3:\t" << exam3Grade << std::endl;
 
     const double labAssigmentCount = 4;
     double labAverage = (lab1Grade + lab2Grade + lab3Grade + lab4Grade) / labAssigmentCount;
-    std::cout << "Lab Average:\t";
-    std::cout << labAverage;
-    std::cout << std::endl;
+    std::cout << "Lab Average (75% of total):\t" << std::fixed << std::setprecision(2) << labAverage << std::endl;
 
     const double examAssignmentCount = 3;
     double examAverage = (exam1Grade + exam2Grade + exam3Grade) / examAssignmentCount;
-    std::cout << "Exam Average:\t";
-    std::cout << examAverage;
-    std::cout << std::endl;
+    std::cout << "Exam Average (25% of total):\t" << std::fixed << std::setprecision(2) << examAverage << std::endl;
+
+    double classAverage = (examAverage * 0.25) + (labAverage * .75);
+    std::cout << "Class Average:\t" << std::fixed << std::setprecision(2) << classAverage << std::endl;
 }
