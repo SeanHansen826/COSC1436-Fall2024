74b8d2a66c5d9947d416260b7a6563b3b0d7101d
Sean Hansen <sean.hansen826@my.tccd.edu>
2024-09-30 22:51:02 -0500

    interestRate & initialLoan while loops working

--- labs/Lab2/LoanCalc/LoanCalc.cpp
+++ labs/Lab2/LoanCalc/LoanCalc.cpp
@@ -1,32 +1,40 @@
-/*  Loan Calculator
-    Sean Hansen
-    COSC-1436-20005 */
+//Loan Calculator
+//Sean Hansen
+//COSC-1436-20005
 
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
+
 int main()
 {
     cout << setw(30) << setfill('-') << "" << setfill(' ') << endl;
     cout << left << setw(10) << "Project" << "Loan Calculator\n" << setw(10) << "By:" << "Sean Hansen\n" << setw(10) << "Class:" << "COSC-1436-20005\n";
     cout << setw(30) << setfill('-') << "" << setfill(' ') << endl;
 
-    int initialLoan;
-    cout << "Input your initial loan amount (between 1 and 1000): ";
-    cin >> initialLoan;
-    if (initialLoan < 1 || initialLoan > 1000)
+    int initialLoan = 0;
+    while (initialLoan < 1 || initialLoan > 1000)
     {
-        cout << "ERROR: Initial loan must be between 1 and 1000\n";
-        
-        cout << "Input Your Initial Loan Amount (Between 1 and 1000): ";
-        cin >> initialLoan; //Need to continue prompting, research Loops!
-    }
+        cout << "Input your initial loan amount (between 1 and 1000): ";
+        cin >> initialLoan;
+
+        if (initialLoan < 1 || initialLoan > 1000)
+            cout << "ERROR: Initial loan must be between 1 and 1000\n";
+    };
+
+    double interestRate = 0;
+    while (interestRate < 1.0 || interestRate > 100.0)
+    {
+        cout << "Input your interest rate (between 1.0 and 100.0): ";
+        cin >> interestRate;
+
+        if (interestRate < 1.0 || interestRate > 100.0)
+            cout << "Error: Interest rate must be between 1.0 - 100.0\n";
+    };
     
-    double interestRate;
-    cout << fixed << setprecision(2);
-    cout << "Input your interest rate (between 1.0 and 100.0): ";
-    cin >> interestRate;
-    interestRate /= 100.0;
-    cout << interestRate; // outputs wrong, values 1.5 as 1.49999999999... why?
+    interestRate /= 100;
+    cout << interestRate;
+
+    //Calculating table of first 12 months of loan ... table output ... for loop ... 
 }
