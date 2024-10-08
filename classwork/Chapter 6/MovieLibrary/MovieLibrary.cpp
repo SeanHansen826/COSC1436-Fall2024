8ca8a5c51c9b2027486cc11a84997622e8c95f3b
Sean Hansen <sean.hansen826@my.tccd.edu>
2024-10-07 19:57:40 -0500

    updated syntax, cleaned code

---
+++ classwork/Chapter 6/MovieLibrary/MovieLibrary.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include<iomanip>
+#include<string>
+
+using namespace std;
+using std::cout;
+using std::cin;
+
+
+struct Movie
+{
+    string Title;
+
+    int RunLength;
+
+    string Description;
+
+    int ReleaseYear;
+
+    bool IsClassic;
+
+    string Genre;
+};
+
+
+enum MenuCommand
+{   
+    AddMovie = 1,
+    EditMovie,
+    DeleteMovie,
+    ViewMovie,
+};
+
+int main()
+{
+    cout << "Movie Library" << endl;
+    cout << "---------------" << endl;
+    cout << "A)dd Movie" << endl;
+    cout << "E)dit Movie" << endl;
+    cout << "D)elete Movie" << endl;
+    cout << "V)iew Movie" << endl;
+
+    MenuCommand menuCommand = static_cast<MenuCommand>(0);
+
+    //get menu command
+    do     
+    {
+        char input;
+        cin >> input;
+
+        switch (input)
+        {
+            case 'A':
+            case 'a': menuCommand = MenuCommand::AddMovie; break;
+
+            case 'E':
+            case 'e': menuCommand = MenuCommand::EditMovie; break;
+
+            case 'D':
+            case 'd': menuCommand = MenuCommand::DeleteMovie; break;
+
+            case 'V':
+            case 'v': menuCommand = MenuCommand::ViewMovie; break;
+
+            default: cout << "Bad input" << endl; break;
+        };
+    } while (menuCommand == 0);
+    cin.ignore();
+
+    //cipher menu command
+    switch (menuCommand)
+    {
+        case MenuCommand::AddMovie:
+        case MenuCommand::EditMovie:
+        case MenuCommand::DeleteMovie:
+        case MenuCommand::ViewMovie: cout << "Not Implemented" << endl; break;
+    };
+    
+    Movie movie;
+
+    //get title
+    do
+    {
+        cout << "Enter a title: ";
+        getline(cin, movie.Title);
+
+        if (movie.Title == "")
+
+            cout << "ERROR: Title is required" << endl;
+    } while (movie.Title == "");
+
+    //get run length
+    do
+    {
+        cout << "Enter run legth (in minutes): ";
+        cin >> movie.RunLength;
+
+            if (movie.RunLength < 0 || movie.RunLength > 1440)
+            {
+                cout << "ERROR: Run length must be between 0 and 1440" << endl;
+            };
+    } while (movie.RunLength < 0 || movie.RunLength > 1440);
+
+    //get year
+    do
+    {
+        cout << "Enter release year (1900+): ";
+        cin >> movie.ReleaseYear;
+
+        if (movie.ReleaseYear < 1900 || movie.ReleaseYear > 2100)
+        {
+            cout << "ERROR: Release year must be between 1900 and 2100" << endl;
+        };
+    } while (movie.ReleaseYear < 1900 || movie.ReleaseYear > 2100);
+
+    //get optional description
+    cout << "Enter optional description: ";
+    cin.ignore();
+    getline(cin, movie.Description);
+
+    //get isClassic
+    bool done = false;
+    while (!done)
+    {
+        char isClassic;
+        cout << "Is this a Classic (Y/N)?";
+        cin >> isClassic;
+
+        switch (isClassic)
+        {
+            case 'Y': 
+            case 'y': movie.IsClassic = true;  done = true; break;
+
+            case 'N': 
+            case 'n': movie.IsClassic = false; done = true; break;
+
+            default:
+            {
+                cout << "ERROR: You must enter Y or N ";
+            };
+        };
+    }
+    cin.ignore();
+
+    //get genre
+    for (int index = 0; index < 5; ++index)
+    {
+        cout << "Enter optional genre " << (index + 1) << ": ";
+
+        string genre;
+        getline(cin, genre);
+        if (genre == "")
+            break;
+
+        movie.Genre += genre + ", ";
+    };
+
+    //display library
+    cout << "------------" << endl;
+    cout << movie.Title << " (" << movie.ReleaseYear << ")" << endl;
+    cout << "Run Length (mins) " << movie.RunLength << endl;
+    
+    cout << "Is Classic? " << (movie.IsClassic ? "Yes" : "No") << endl;
+    if (movie.Genre != "")
+        cout << "Genre(s) " << movie.Genre << endl;
+    if (movie.Description != "")
+        cout << movie.Description << endl;
+    cout << "------------" << endl;
+}
