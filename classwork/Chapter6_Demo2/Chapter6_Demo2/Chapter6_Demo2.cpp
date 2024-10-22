80a420a131f72ef6d87ea0391950439d25ff4236
Sean Hansen <sean.hansen826@my.tccd.edu>
2024-10-21 20:42:57 -0500

    Chapter6.2

---
+++ classwork/Chapter6_Demo2/Chapter6_Demo2/Chapter6_Demo2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+using std::cout;
+using std::cin;
+
+//Funtion overloading - 2 or more functions have the same name but different signatures
+
+//Overload Resoltuion - essentially an if else if where the compiler decides with overloaded function to use
+//      looks at how many parameters first, and then chooses the exact types, then types that can be coerced up! (int => float)
+// 
+//      For overloading make sure each "overload" does the same thing
+//  
+//  Function Signature -- function name + the argument types            ***QUIZ***
+
+
+//
+
+/// @brief Reads an integer from console
+/// @param message Message to display
+/// @return Integral value provided by user
+int ReadInt(string message)
+{
+    //Display Message
+    //Get int value and return
+
+    cout << message;
+
+    int value;
+    cin >> value;
+
+    return value;
+}
+
+int ReadInt(string message, int minValue, int maxValue)
+{
+    int value;
+
+    do
+    {
+        value = ReadInt(message);
+
+        if (value < minValue || value > maxValue)
+            cout << "value must be between " << minValue << " & " << maxValue;
+
+    } while (value < minValue || value > maxValue);
+
+    return value;
+}
+
+int ReadInt(string message, int minValue)
+{
+    return ReadInt(message, minValue, INT32_MAX);        //INT32_MAX provides a value for the parameter that we're not needing, basically a placeholder
+}
+
+int main()
+{
+
+    int value = ReadInt ( "Enter a value between 1 - 10", 1, 10 );
+
+    int positive = ReadInt("Enter a positive Number: ", 0);
+
+    int anyValue = ReadInt("Enter a number: ");
+
+    int test = ReadInt("a message", 10, 50);
+}
+
