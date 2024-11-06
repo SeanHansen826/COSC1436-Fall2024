c5292219e9573e781d8640af2684605b146b2e71
Seanhansen826 <sean.hansen826@my.tccd.edu>
2024-11-06 13:23:12 -0600

    added GetSum

--- labs/Lab4/Lab4/Lab4.cpp
+++ labs/Lab4/Lab4/Lab4.cpp
@@ -77,10 +77,22 @@ void PromptArrayInput( int valueArray[], const int MaxValues, const int MinValue
     }
 }
 
+void GetSum(int valueArray[], int count)
+{
+    int sumOfArray = 0;
+
+    for (int index = 0; index < count; ++index)
+    {
+        sumOfArray += valueArray[index];
+    }
+
+    cout << "* Sum of Array: " << sumOfArray << " *" << endl << endl;
+}
+
 /// @brief Calculates largest value of valueArray
 /// @param valueArray passes value array
 /// @param count passes size which is equal to the number of array entries
-void GetLargestValue( int valueArray[], int count, const int MinValues )
+void GetLargest( int valueArray[], int count, const int MinValues )
 {
     int largestValue = MinValues;
     
@@ -93,7 +105,11 @@ void GetLargestValue( int valueArray[], int count, const int MinValues )
     cout << "* Largest Value: " << largestValue << " *" << endl << endl;
 }
 
-void GetSmallestValue(int valueArray[], int count, const int MaxValues)
+/// @brief Gets smallest value
+/// @param valueArray calls array
+/// @param count calls count for total array values
+/// @param MaxValues sets maxValues as the smallestValue while (count != 0)
+void GetSmallest(int valueArray[], int count, const int MaxValues)
 {
     int smallestValue = MaxValues;
 
@@ -102,8 +118,8 @@ void GetSmallestValue(int valueArray[], int count, const int MaxValues)
         if (valueArray[index] < smallestValue)
             smallestValue = valueArray[index];
     }
-
-    cout << "* Smallest Value: " << smallestValue << " *" << endl << endl;
+    
+    cout << "* Smallest Value: " << smallestValue << " *" << endl << endl;   
 }
 
 /// @brief Displays main menu, Gets desired Calculation
@@ -116,36 +132,49 @@ bool DisplayMenu(int valueArray[], const int MaxValues, const int MinValues, int
 {
     cout << setw(14) << "Main Menu\n";
     cout << setfill('-') << setw(17) << "" << setfill(' ') << endl;
-    cout << "L) argest Value\n";
-    cout << "S) mallest Value\n";
+    cout << "A) dd\n";
+    cout << "L) argest\n";
+    cout << "S) mallest\n";
     cout << "Q) uit\n";
     cout << endl;
 
     bool done = false;
     char menuInput;
     bool quit = false;
-    do
+
+    if(count != 0)
     {
-        cout << "Choose Calculation Type: ";
-        cin >> menuInput;
-
-        switch (menuInput)
+        do
         {
-            case 'l':
-            case 'L': GetLargestValue(valueArray, count, MinValues); cout << endl; done = true; return quit; break;
+            if(count != 0)
+            cout << "Choose Calculation Type: ";
+            cin >> menuInput;
 
-            case 'S':
-            case 's': GetSmallestValue(valueArray, count, MaxValues); cout << endl; done = true; return quit; break;
-
-            case 'Q':
-            case 'q': quit = true; done = true; return quit; break;
-
-            default:
+            switch (menuInput)
             {
-                cout << "ERROR: Must input valid menu command" << endl;
+                case 'A':
+                case 'a': GetSum(valueArray, count); cout << endl; done = true; return quit; break;
+
+                case 'L':
+                case 'l': GetLargest(valueArray, count, MinValues); cout << endl; done = true; return quit; break;
+
+                case 'S':
+                case 's': GetSmallest(valueArray, count, MaxValues); cout << endl; done = true; return quit; break;
+
+                case 'Q':
+                case 'q': quit = true; done = true; return quit; break;
+
+                default:
+                {
+                    cout << "ERROR: Must input valid menu command" << endl;
+                }
             }
-        }
-    } while (!done);
+        } while (!done);
+    }
+    else if (count == 0)
+    {
+        cout << "* NO ARRAY INPUTS *" << endl << endl;
+    }
 }   //not all control paths return a value (?)
 
 void DisplayQuitMessage()
